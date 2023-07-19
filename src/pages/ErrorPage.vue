cc4dd40897e55aa6e55f15b06c77929e2ce6af7c
Marco Giammari <marcogiammari@gmail.com>
2023-07-19 17:49:22 +0200

    feat: ErrorPage (testing)

---
+++ src/pages/ErrorPage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "ErrorPage",
+}
+</script>
+
+<template>
+    <div class="text-center py-48">
+        <h2 class="text-3xl">ERROR: {{ $route.params.code }}</h2>
+        <h4>Something went wrong...</h4>
+    </div>
+</template>
